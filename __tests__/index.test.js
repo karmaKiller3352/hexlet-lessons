566e1baddd77c1281da513b8226d3a339e6c303b
Suleiman <alievsul@gmail.com>
2020-02-15 01:43:26 +0200

    yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import reverse from '../src';
 
 test('reverse', () => {
-  expect(reverse('hello')).toEqual('ollleh');
+  expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
 });
